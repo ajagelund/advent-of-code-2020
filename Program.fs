50ce3e4ee41ff0a8420279cf5db15b9303ab6a90
Andreas Jagelund <andreas@jagelund.se>
2020-12-03 10:33:05 +0100

    WIP

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open Common.FileHandling
 
 // Define a function to construct a message to print
 let from whom =
@@ -10,4 +11,14 @@ let from whom =
 let main argv =
     let message = from "F#" // Call the function
     printfn "Hello world %s" message
+
+    let content = readLines ".\\files\\2001-1.txt"
+
+    let list = Seq.map (fun x -> int x) content
+    
+    Seq.sort list 
+        |> Seq.iter (printf "%s\n")
+
+    // for line in content.sort do
+    //     printfn "%s" line
     0 // return an integer exit code
